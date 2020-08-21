b4628a7821b32bc30182084e1349931a2cb35e71
Rinat <lich666black@gmail.com>
2020-08-21 11:46:25 +0500

    added rpc methods

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import { BlockChain } from './common';
 import App from './server';
-import { PORT } from './config';
 
 export function main() {
-    // new BlockChain();
-    App.listen(PORT, () => {
-        console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
-    });
+    new BlockChain();
+    App();
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import { PORT } from '../config';
 
 const app = express();
 
 app.get('/', (req, res) => res.send('test'));
 
-export default app;
+export default function() {
+    app.listen(PORT, () => {
+        console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+    });
+};
