b135710db35751b3c199da63b0ee47b32231847c
Rinat <lich666black@gmail.com>
2020-08-21 15:53:33 +0500

    added getBlockchaininfo

---
+++ src/server/routers/get-blockchain-info.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import { BlockChain } from '../../common';
+
+export default function(req: Request, res: Response) {
+    const { body } = req;
+    const chain = req.app.settings.chain as BlockChain;
+    const lastDSBlock = chain.getLastBlock;
+
+    return res.json({
+        id: body.id,
+        jsonrpc: body.jsonrpc,
+        result: {
+            CurrentDSEpoch: String(lastDSBlock.blockHeader.blockNum),
+            CurrentMiniEpoch: "0",
+            DSBlockRate: 1,
+            NumDSBlocks: String(chain.dsBlocks.size),
+            NumPeers: 0,
+            NumTransactions: "4350627",
+            NumTxBlocks: String(chain.txBlocks.size),
+            NumTxnsDSEpoch: "748",
+            NumTxnsTxEpoch: "0",
+            ShardingStructure: {
+                NumPeers: [0]
+            },
+            TransactionRate: 100,
+            TxBlockRate: 10
+        }
+    });
+};
