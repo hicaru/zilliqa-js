3b9c5e04a44c8366318a5aaa4c525fcb85b059bf
Rinat <lich666black@gmail.com>
2020-08-25 14:16:51 +0500

    added Storage abstract class

--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -1,2 +1,11 @@
-export class Storage {
+export abstract class Storage {
+    abstract getDSBlockNumber(blockNumber: number | string): void;
+    abstract getTXBlockNumber(blockNumber: number | string): void;
+    abstract getTX(hash: string): void;
+
+    abstract setNewDSBlock(): void;
+    abstract setNewTXBlock(): void;
+}
+
+export class MemmoryStorage extends Storage {
 }
