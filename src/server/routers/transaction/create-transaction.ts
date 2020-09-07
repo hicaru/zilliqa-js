958999acfad31b9afcdbc324750350ef5d0fc70a
Rinat <lich666black@gmail.com>
2020-09-07 11:40:16 +0500

    fixed create-transaction

--- src/server/routers/transaction/create-transaction.ts
+++ src/server/routers/transaction/create-transaction.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { getAddressFromPublicKey } from '@zilliqa-js/crypto/dist/util';
 import { BlockChain, Transaction } from '../../../common';
-import { invalidParams, unableToVerifyTransaction } from '../../errors';
+import { invalidParams, unableToVerifyTransaction, RPCErrorCode } from '../../errors';
 
 export default function(req: Request, res: Response) {
     const { body } = req;
@@ -30,6 +30,30 @@ export default function(req: Request, res: Response) {
             attributes.priority
         );
 
+        if (account.nonce >= transaction.nonce) {
+            return res.json({
+                id: body.id,
+                jsonrpc: body.jsonrpc,
+                error: {
+                    data: null,
+                    code: RPCErrorCode.RPC_INVALID_PARAMETER,
+                    message: `Nonce (${transaction.nonce}) lower than current (${account.nonce})`
+                }
+            });
+        }
+
+        if (transaction.signature.length != 128) {
+            return res.json({
+                id: body.id,
+                jsonrpc: body.jsonrpc,
+                error: {
+                    data: null,
+                    code: RPCErrorCode.RPC_INVALID_PARAMETER,
+                    message: 'Invalid Signature size'
+                }
+            });
+        }
+
         if (!transaction.isValid()) {
             return res.json(unableToVerifyTransaction(body.id, body.jsonrpc));
         }
@@ -45,6 +69,6 @@ export default function(req: Request, res: Response) {
             }
         });
     } catch (err) {
-        return res.json(unableToVerifyTransaction(body.id, body.jsonrpc));
+        return res.json(invalidParams(body.id, body.jsonrpc));
     }
 };
